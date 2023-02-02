aa49f9c50c29080445b82971eec0b8f8c0bba33f
Bruno Agatao <bagatao@anaconda.com>
2023-02-03 00:13:54 +0100

    Fix: rename test files

--- src/__tests__/jupyterlab_anaconda_analytics.spec.ts
+++ src/__tests__/jupyterlab_anaconda_analytics.spec.ts
@@ -2,7 +2,7 @@
  * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
  */
 
-describe('jupyterlab-google-analytics', () => {
+describe('jupyterlab-anaconda-analytics', () => {
   it('should be tested', () => {
     expect(1 + 1).toEqual(2);
   });
