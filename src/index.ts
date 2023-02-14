a26b3e883c4309b8763627e020abe381a4a5d477
Bruno Agatao <bagatao@anaconda.com>
2023-02-14 10:10:10 +0100

    Add gtag function to return token

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,25 @@ import {
 } from '@jupyterlab/application';
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { Token } from '@lumino/coreutils';
 
 import { ScriptMetadata } from './config';
 import { loadApp } from './utils';
+
+export interface IAnalytics {
+  gtag: ((...args: any[]) => void) | null;
+}
+
+export const IAnalytics = new Token<IAnalytics>(
+  'jupyterlab-anaconda-analytics:plugin:IAnalytics'
+);
 
 /**
  * Initialization data for the jupyterlab-anaconda-analytics extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-anaconda-analytics:plugin',
+  provides: IAnalytics,
   autoStart: true,
   requires: [ISettingRegistry],
   activate: async (
@@ -29,7 +39,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
       // Get heap analytics tracking id
       const heapAnalytics = settings.get('heap').composite as string;
 
-      // push the tracking id's into the array
+      // Push the tracking ids into the array
       scriptMetaData.push(
         { id: 'ga', secret: gaAnalytics },
         { id: 'heap', secret: heapAnalytics }
@@ -38,8 +48,18 @@ const plugin: JupyterFrontEndPlugin<void> = {
       scriptMetaData.forEach((app: ScriptMetadata) => {
         loadApp(app.id, app.secret);
       });
+
+      // Activate GA's global site tag
+      const w = window as any;
+      w.dataLayer = w.dataLayer || [];
+      w.gtag = function () {
+        w.dataLayer.push(arguments);
+      };
+
+      return { gtag: w.gtag };
     } catch (err) {
       console.error(err);
+      return null;
     }
   }
 };
